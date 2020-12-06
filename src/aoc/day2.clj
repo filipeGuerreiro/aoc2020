4b17013f1d4d2262c324bfae3b80543deda4671f
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2020-12-06 12:56:02 +0900

    Add day2 implementation

--- src/aoc/day2.clj
+++ src/aoc/day2.clj
@@ -1,9 +1,36 @@
-(ns aoc.day2)
+(ns aoc.day2
+  (:require [clojure.string :as str]))
 
+(defn- get-lines [input]
+  (str/split-lines input))
+(defn- valid-password [line policy]
+  (let [[r ch pw] (str/split line #" ")
+        [lo hi]   (str/split r #"-")
+        ch        (first ch)
+        pwparts   (group-by identity (seq pw))
+        num       (count (pwparts ch))]
+    (policy pw ch num hi lo)))
+(defn- count-valid-lines
+  [in policy]
+  (->> in
+       get-lines
+       (filter #(valid-password % policy))
+       (count)))
+
+(defn- part-1-policy
+  [pw ch num hi lo]
+  (and (<= num (Integer/parseInt hi)) (>= num (Integer/parseInt lo))))
 (defn part-1
   [in]
-  in)
+  (count-valid-lines in part-1-policy))
 
+(defn- part-2-policy
+  [pw ch num hi lo]
+  (let [pwparts (vec (seq pw))
+        chlo    (= ch (pwparts (dec (Integer/parseInt lo))))
+        chhi    (= ch (pwparts (dec (Integer/parseInt hi))))]
+    (and (or chlo chhi)
+         (not (and chlo chhi)))))
 (defn part-2
   [in]
-  in)
+  (count-valid-lines in part-2-policy))
