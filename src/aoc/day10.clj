fccf7711bcc478f9f2a7927c08a461a94132a362
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2020-12-20 10:03:48 +0900

    Day 10 part 2

--- src/aoc/day10.clj
+++ src/aoc/day10.clj
@@ -13,17 +13,34 @@
         diffs
         (->> window
              (map (fn [e] (- e pivot)))
-             (map (fn [e] {e 1}))        ; '({1 1} {1 1} {3 1})
+             (map (fn [e] {e 1}))         ; '({1 1} {1 1} {3 1})
              (apply merge-with +)        ; {1 2} {3 1}
              (merge-with + diffs)
              (recur (drop 1 adapters')))))))
+(defn- parse
+  [in]
+  (let [sorted-lines (sort (map #(Integer/parseInt %) (str/split-lines in)))]
+    (concat '(0) sorted-lines (list (+ 3 (apply max sorted-lines))))))
 (defn part-1
   [in]
-  (let [adapters (sort (map #(Integer/parseInt %) (str/split-lines in)))
+  (let [adapters (parse in)
         diffs (count-diffs adapters)]
     (spy diffs)
     (* (get diffs 1) (get diffs 3))))
 
+(defn- count-combinations-solver [nums]
+  (def solver
+    (memoize
+      (fn [i]
+        (if (zero? i)
+          1
+          (let [js  (filter #(<= 0 (- i %)) (range 1 4))
+                js' (filter #(<= (- (nums i) (nums (- i %))) 3) js)]
+            (apply + (map #(solver (- i %)) js'))))))))
+(defn- count-combinations [nums]
+  (let [solver (count-combinations-solver nums)]
+    (solver (dec (count nums)))))
 (defn part-2
   [in]
-  in)
+  (let [adapters (parse in)]
+    (count-combinations adapters)))
